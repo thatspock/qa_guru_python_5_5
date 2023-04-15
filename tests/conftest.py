17d93a1abe6f2746b150ae049b456fc8c5a1fbe6
Some Name <thatspock@gmail.com>
2023-04-15 14:16:50 +0700

    create conftest with data

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
